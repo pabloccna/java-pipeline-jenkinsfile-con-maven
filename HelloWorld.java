0082483808172d40a57854cc9ebf98d9eebb44ba
pabloccna <52111134+pabloccna@users.noreply.github.com>
2020-03-28 01:02:49 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+ 
+	public static void main(String[] args) {
+		int i=666;
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Hola Mundo...... y entendiendo el webhook");
+		System.out.println("el valor de i: "+i);
+		System.out.println("Ahora hay un: Jenkinsfile + HelloWorld.java");
+		System.out.println("-----------------------------------------------------------");
+	}
+}
